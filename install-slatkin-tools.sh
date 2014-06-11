1caa8bd33f0aa716feeb1a4118870a900af843e6
Mark Madsen <mark@madsenlab.org>
2014-06-10 17:36:41 -0700

    Stop making dumb errors updating version numbers!

--- install-slatkin-tools.sh
+++ install-slatkin-tools.sh
@@ -3,5 +3,5 @@ set -ex
 cd /tmp
 wget --no-check-certificate  https://github.com/mmadsen/slatkin-exact-tools/archive/1.1.tar.gz  -O /tmp/slatkin-exact-tools-1.1.tar.gz
 tar -xzvf slatkin-exact-tools-1.1.tar.gz
-cd slatkin-exact-tools-1.0 && make && make install
+cd slatkin-exact-tools-1.1 && make && make install
 
