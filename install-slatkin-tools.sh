b8f99f8cb91677731fff0361202ff9de984c56bb
Mark Madsen <mark@madsenlab.org>
2014-06-10 17:34:02 -0700

    New version of slatkin with -lm on linux

--- install-slatkin-tools.sh
+++ install-slatkin-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 cd /tmp
-wget --no-check-certificate  https://github.com/mmadsen/slatkin-exact-tools/archive/1.0.tar.gz  -O /tmp/slatkin-exact-tools-1.0.tar.gz
-tar -xzvf slatkin-exact-tools-1.0.tar.gz
+wget --no-check-certificate  https://github.com/mmadsen/slatkin-exact-tools/archive/1.1.tar.gz  -O /tmp/slatkin-exact-tools-1.1.tar.gz
+tar -xzvf slatkin-exact-tools-1.1.tar.gz
 cd slatkin-exact-tools-1.0 && make && make install
 
